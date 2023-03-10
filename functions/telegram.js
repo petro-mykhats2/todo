e19892fa1670be71af2aa5b3b8d8cab2ead096b3
petro-mykhats2 <02webproject02@gmail.com>
2023-03-10 16:40:39 +0100

    test telegram bot 3

--- functions/telegram.js
+++ functions/telegram.js
@@ -4,10 +4,18 @@ const token = "YOUR_TELEGRAM_BOT_TOKEN"
 const bot = new TelegramBot(token, { polling: false })
 
 exports.handler = async (event) => {
-  const message = JSON.parse(event.body)
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" }
+  }
+
+  if (!event.body) {
+    return { statusCode: 400, body: "Body is empty!" }
+  }
   const chatId = 735449634
 
+  let message
   try {
+    const message = JSON.parse(event.body)
     await bot.sendMessage(chatId, message)
     return {
       statusCode: 200,
